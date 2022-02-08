b6826a41910967c9001535eb3136489636343129
tommy <bt@g>
2022-02-08 15:16:32 -0800

    redid logic

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ let divide = (a, b) => a / b;
 
 let inputs = document.querySelector('.inputs');
 let output = document.querySelector('.output');
-let firstNum;
-let secondNum;
-let operator;
+let firstNum = '';
+let secondNum = '';
+let symbol = '';
 
 // operate gets called when the = button is clicked
 function operate(operator, operand1, operand2) {
@@ -27,30 +27,27 @@ buttonList.forEach((button) => {
 });
 
 function logTest (e) {
-  // firstNum = e.target.textContent;
-  // console.log(firstNum);
-  // inputs.textContent += firstNum;
-  
-  let input = e.target.textContent; 
-  // firstInput = whatever # btn was pressed. ignore ops
-   if (!isNaN(input) || input === '.') { //working period
-     console.log(input);
-   };
-  // when an op is clicked, assign it to operand. clicking another op reassigns it
+  //numTemp += whatever #s are pressed
+  if (e.target.className.includes('num') && !secondNum && !symbol) {
+    firstNum += e.target.textContent;
+    console.log(firstNum);
+  };
+  //display numtemp at top
+  // when an op is clicked, assign numTemp to firstNum, assign op to symbol.
+  if (!e.target.className.includes('num') && firstNum && !secondNum) {
+    symbol = e.target.className
+    console.log(`the current operator is ${e.target.className}`);
+  }
   // if op has a value and a num is clicked, assign it to secondNum
   // when an op or = is clicked after 2 #s exist, 
   // display it as result (or result AND firstNum if op was used and not =)
-
   
 
+};
 
 
   // console.log(e.target.className);
   // console.log(e.target.textContent)
-};
-
-
-
 
 
 
