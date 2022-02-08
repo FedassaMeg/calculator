6ba541369ad12055aaa226a95dbfb72e8692e70e
tommy <bt@g>
2022-02-08 14:14:05 -0800

    added dot functionality

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
 
 let inputs = document.querySelector('.inputs');
-let currentNum;
+let output = document.querySelector('.output');
+let firstNum;
+let secondNum;
+let operator;
 
 // operate gets called when the = button is clicked
 function operate(operator, operand1, operand2) {
@@ -24,12 +27,24 @@ buttonList.forEach((button) => {
 });
 
 function logTest (e) {
-  currentNum = e.target.textContent;
-  console.log(currentNum);
-  inputs.textContent += currentNum;
-
+  // firstNum = e.target.textContent;
+  // console.log(firstNum);
+  // inputs.textContent += firstNum;
+  
+  let input = e.target.textContent; 
+  // firstInput = whatever # btn was pressed. ignore ops
+   if (!isNaN(input) || input === '.') { //working period
+     console.log(input);
+   };
+  // when an op is clicked, assign it to operand. clicking another op reassigns it
+  // if op has a value and a num is clicked, assign it to secondNum
+  // when an op or = is clicked after 2 #s exist, 
+  // display it as result (or result AND firstNum if op was used and not =)
 
   
+
+
+
   // console.log(e.target.className);
   // console.log(e.target.textContent)
 };
