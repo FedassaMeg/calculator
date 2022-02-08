c613c6f000da4d409f02acef76d61c9cc7f61997
tommy <bt@g>
2022-02-08 13:37:14 -0800

    working number buttons

--- script.js
+++ script.js
@@ -3,8 +3,42 @@ let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
 
+let inputs = document.querySelector('.inputs');
+let currentNum;
+
 // operate gets called when the = button is clicked
 function operate(operator, operand1, operand2) {
   return operator(operand1, operand2);
 };
 
+
+
+
+
+
+
+buttonList = document.querySelectorAll('button');
+
+buttonList.forEach((button) => {
+  button.addEventListener('click', logTest);
+});
+
+function logTest (e) {
+  currentNum = Number(e.target.textContent);
+  console.log(currentNum);
+  inputs.textContent = currentNum;
+  // console.log(e.target.className);
+  // console.log(e.target.textContent)
+};
+
+
+
+
+
+
+
+function displayNum() {
+  // when a num button is pressed
+  // assign the corresponding num to a value
+  // make the display show the value 
+};
