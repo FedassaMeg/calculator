f235763d83f13538a8df72bd258d845b42833e0e
tommy <bt@g>
2022-02-08 17:32:20 -0800

    working calculator barely

--- script.js
+++ script.js
@@ -12,29 +12,31 @@ let answer = 0;
 let displayEq = '';
 
 // operate gets called when the = button is clicked
+// this is a requirement of the homework assignment so I have to use it
 function operate(operator, operand1, operand2) {
   return operator(operand1, operand2);
 };
 
-
-
 buttonList = document.querySelectorAll('button');
 
 buttonList.forEach((button) => {
-  button.addEventListener('click', logTest);
+  button.addEventListener('click', calculate);
 });
 
-function logTest (e) {
+function calculate (e) {
+  // this function is a series of ifs that progress the stages of calcultor use
+  // the ifs check if the first operand, second operand, or operator exist to determine what to execute
 
   let input = e.target.className
 
+  // if the input was a number button, and the second number and operator don't exist:
   if (input.includes('num') && !secondNum && !symbol) {
     firstNum += e.target.textContent;
     console.log(firstNum);
     inputs.textContent = firstNum;
   };
-  //display numtemp at top
-  // when an op is clicked, assign numTemp to firstNum, assign op to symbol.
+
+  // if the input was an operator button:
   if (!input.includes('num') && firstNum && !secondNum) {
     if (symbol) {
       inputs.textContent = inputs.textContent.replace(/.$/,e.target.textContent);
@@ -44,10 +46,19 @@ function logTest (e) {
     symbol = e.target.className
     console.log(`the current operator is ${e.target.className}`);
   }
+  // if the input is a number, and the first number and operator already exist
   if (input.includes('num') && firstNum && symbol) {
     secondNum += e.target.textContent;
     console.log(secondNum);
     inputs.textContent += e.target.textContent;
+  }
+
+  if (e.target.className.includes('clear')) {
+    output.textContent = '';
+    symbol = '';
+    firstNum = '';
+    secondNum = '';
+    inputs.textContent = '';
   }
  
 
@@ -57,22 +68,26 @@ function logTest (e) {
     case 'add':
       answer = operate(add, Number(firstNum), Number(secondNum));
       console.log(answer);
+      output.textContent = answer;
       break;
     case 'sub':
       answer = operate(sub, Number(firstNum), Number(secondNum));
       console.log(answer);
+      output.textContent = answer;
       break;
     case 'multiply':
       answer = operate(multiply, Number(firstNum), Number(secondNum));
       console.log(answer);
+      output.textContent = answer;
       break;
     case 'divide':
       answer = operate(divide, Number(firstNum), Number(secondNum));
       console.log(answer);
+      output.textContent = answer;
       break;
     default:
       console.log(`Sorry, we encountered a bug.`);
-}
+    }
   };
   // if op has a value and a num is clicked, assign it to secondNum
   // when an op or = is clicked after 2 #s exist, 
